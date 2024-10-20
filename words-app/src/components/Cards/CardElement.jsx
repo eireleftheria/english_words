85351b9bfc5a660c851711517e9346b7a1c614c1
Irina-eleftheria <ikozlova_work@inbox.ru>
2024-10-20 13:03:05 +0300

    add button into cards

--- words-app/src/components/Cards/CardElement.jsx
+++ words-app/src/components/Cards/CardElement.jsx
@@ -1,11 +1,25 @@
 import styles from "../Cards/CardElement.module.css";
+import React, { useState } from "react";
 
 function CardElement({ english, transcription, russian }) {
+  const [isSelected, setSelected] = useState(true);
+
   return (
     <div className={styles.cardElement_container}>
       <p>{english}</p>
       <p>{transcription}</p>
-      <p>{russian}</p>
+      {isSelected ? (
+        <button
+          className={styles.checkButton}
+          onClick={() => setSelected(false)}
+        >
+          Проверить
+        </button>
+      ) : (
+        <p className={styles.translatedWord} onClick={() => setSelected(true)}>
+          {russian}
+        </p>
+      )}
     </div>
   );
 }
--- words-app/src/components/Table/TableRow.jsx
+++ words-app/src/components/Table/TableRow.jsx
@@ -1,13 +1,68 @@
+import React, { useState } from "react";
 import styles from "./TableRow.module.css";
 import Button from "../Button/Button";
 
 function TableRow({ id, english, transcription, russian }) {
+  const [isSelected, setSelected] = useState(false);
+  const [valueEnglish, setValueEnglish] = useState(english);
+  const [valueTranscription, setValueTranscription] = useState(transcription);
+  const [valueRussian, setValueRussian] = useState(russian);
+
+  function handleEnglishChange(event) {
+    setValueEnglish(event.target.value);
+  }
+
+  function handleTranscriptionChange(event) {
+    setValueTranscription(event.target.value);
+  }
+
+  function handleRussianChange(event) {
+    setValueRussian(event.target.value);
+  }
+
   return (
     <tr className={styles.table_rows}>
       <td>{id}</td>
-      <td>{english}</td>
-      <td>{transcription}</td>
-      <td>{russian}</td>
+
+      <td>
+        {isSelected ? (
+          <input
+            type="text"
+            onChange={handleEnglishChange}
+            value={valueEnglish}
+            onBlur={() => setSelected(false)}
+          />
+        ) : (
+          <td onClick={() => setSelected(true)}>{valueEnglish}</td>
+        )}
+      </td>
+
+      <td>
+        {isSelected ? (
+          <input
+            type="text"
+            onChange={handleTranscriptionChange}
+            value={valueTranscription}
+            onBlur={() => setSelected(false)}
+          />
+        ) : (
+          <td onClick={() => setSelected(true)}>{valueTranscription}</td>
+        )}
+      </td>
+
+      <td>
+        {isSelected ? (
+          <input
+            type="text"
+            onChange={handleRussianChange}
+            value={valueRussian}
+            onBlur={() => setSelected(false)}
+          />
+        ) : (
+          <td onClick={() => setSelected(true)}>{valueRussian}</td>
+        )}
+      </td>
+
       <td>
         <Button text="Edit" colorVar="edit"></Button>
         <Button text="Delete" colorVar="delete"></Button>
